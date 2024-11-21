85e2b7f172ab02d4b57e439c3358626b0e497190
issac-cosmos <rud____@naver.com>
2024-11-21 20:39:31 +0900

    join

--- 8.join.sql
+++ 8.join.sql
@@ -41,7 +41,59 @@ select distinct a.* from post p inner join author a on p.author_id=a.id;
 select * from author a where id in (select author_id from post);
 --select절 안에 서브쿼리
 --author의 email과 author별로 본인이 쓴 글의 개수를 출력
-select a.email, (select count(*)) from post where post_count; --하다가 다못적음
+select a.email, (select count(*) from post where author_id=a.id) from author a;
 
 --from절 안에 서브쿼리
 select a.name from (select * from author) as a;
+
+--프러그래머스) 없어진 기록 찾기
+SELECT ANIMAL_OUTS.ANIMAL_ID,ANIMAL_OUTS.NAME from ANIMAL_OUTS where ANIMAL_ID not in (select ANIMAL_ID from ANIMAL_INS) ;
+--서브쿼리, join
+
+
+--집계함수
+--null은 count()함수에서 제외
+select count(*) from author;
+select sum(price) from post;
+select avg(price) from post;
+-- 소수점 첫번쨰자리에서 반올림해서 소수점을 없앰
+select round(avg(price), 0) from post;
+
+
+--group by : 그룹화된 데이터를 하나의 행(row)처럼 취급.
+--author_id로 그룹핑 하였으면, 그외의 컬럼을 조회하는것은 적절치 않음.
+select author_id from post group by author_id;
+--group by와 집계함수
+-- 아래 쿼리에서 *은 그룹화된 데이터내에서의 개수
+select author_id, count(*) from post group by author_id;
+select author_id, count(*), sum(price) from post group by author_id;
+
+
+--author의 email과 author별로 본인이 쓴 글의 개수를 출력
+select a.email, (select count(*) from post where author_id=a.id) from author a;
+--join과 group by, 집계함수 활용한 글의 개수 출력
+select a.email, count(p.author_id) from author a left join post p on a.id=p.author_id
+group by a.id;--group by 뒤에 a.email 이 더좋을듯하다.
+
+--where와 proup by
+--연도별 post 글의 개수 출력, 연도가 null인 값은 제외
+select date_format(created_time ,'%y') as year , count(author_id) 
+from post where post.created_time is not null group by year;
+
+--프로그래머스 문제
+--자동차 종류별 특정 옵션이 포함된 자동차수 구하기
+--입양시각구하기
+
+--having : group by를 통해 나온 집계값에 대한 조건
+--글을 2개 이상 쓴사람에 대한 정보조회
+select author_id from post group by author_id having count(*)>=2;
+select author_id count(*) as count from post group by author_id having count>=2;
+
+--동명동물수찾기
+
+--다중열 group by 
+--post에서 작성자가 만든 제목의 개수를 출력하시오
+select author_id,title, count(*) from post group by author_id, title;
+
+--재구매가 일어난 상품과 회원라스트 구하기
+
